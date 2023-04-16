0219598a0a0414420bf86a7f4f03606ae1ef9bba
AndyOoh <andy.oeee@gmail.com>
2023-04-16 18:04:34 +0700

    add custom tw colors

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx}',
     // include packages if not transpiling - IMPORTANT for uisng ui package when transpiling it inside apps
-    "../../packages/**/*.{js,ts,jsx,tsx}",
+    '../../packages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
@@ -15,11 +15,25 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     themes: [
+      {
+        eventDee: {
+          ...require('daisyui/src/colors/themes')['[data-theme=coffee]'],
+          primary: '#F9F8F5', // white-ish
+          // "primary-focus": "mediumblue",
+          secondary: '#F2EFE8', // beige
+          accent: '#f9b1cc',
+          neutral: '#1C161D',
+          'base-100': '#F9F8F5', // almmost white
+          info: '#87B1E8',
+          success: '#7FE6A7',
+          warning: '#FAA447',
+          error: '#EA5766',
+        },
+      },
       'coffee',
-      'synthwave',
-      'cupcake',
-      'light',
+      // 'synthwave',
+      // 'cupcake',
+      // 'light',
     ],
   },
-}
-
+};
--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export const Button = () => {
-  return <button className='btn btn-accent'>Orange div from ui package</button>;
+  return <button className='btn btn-secondary'>Orange div from ui package</button>;
 };
