fd892ba57bf087a0175d361616c71324c71a45e5
AndyOoh <60953822+AndyOooh@users.noreply.github.com>
2023-04-19 00:29:46 +0700

    Setup/GitHub actions (#5)
    * migrate push any build
    
    * deploy on push to main script
    
    * vercel.json - disable main deploy
    
    * test deploy script
    
    * add release deploy script
    
    * update landing header

--- apps/landing/src/components/Header/HeaderRight.tsx
+++ apps/landing/src/components/Header/HeaderRight.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import Link from 'next/link';
+
 type Props = {};
 
 function HeaderRight({}: Props) {
   return (
     <>
       <div className='flex gap-2 navbar-end'>
-        <button className='btn btn-outline'>Log In</button>
-        <button className='btn'>Sign Up</button>
+        <Link role='button' href={'signup/business'} className='btn btn-sm rounded-3xl normal-case'>
+          Hire Talent
+        </Link>
       </div>
     </>
   );
