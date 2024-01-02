5c0708d9cfe8ef02d5826ddfa593e9f7a2dbf4ab
AndyOoh <andy.oeee@gmail.com>
2024-01-02 23:19:43 +0700

    set up places

--- apps/app/src/app/(protected)/events/create/form/event-location/MapLocation.tsx
+++ apps/app/src/app/(protected)/events/create/form/event-location/MapLocation.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { GoogleMap } from '@react-google-maps/api';
+import { Places } from './places';
 
 type Props = {};
+type LatLngLiteral = google.maps.LatLngLiteral;
+type MapOptions = google.maps.MapOptions;
 
 export const MapLocation = (props: Props) => {
-  return <div>Map</div>;
+  const [office, setOffice] = useState<LatLngLiteral>();
+  const mapRef = useRef<GoogleMap>();
+  // const bangkokCords = useMemo(() => ({ lat: 40, lng: -80 }), []);
+  const bangkokCords = useMemo<LatLngLiteral>(() => ({ lat: 13.736717, lng: 100.523186 }), []);
+  const options = useMemo<MapOptions>(
+    () => ({
+      mapId: 'b34786d7e100891b',
+      disableDefaultUI: true,
+      zoomControl: true,
+    }),
+    []
+  );
+
+  const onload = useCallback(map => {
+    mapRef.current = map;
+  }, []);
+
+  return (
+    <div className='w-4/5 h-[50rem] mx-auto flex flex-col'>
+      <div className='w-full'>Hej Hej Hej</div>
+      <div className='input'>
+        {/* <div className='first:bg-yellow-400 first-of-type:bg-cyan-400 p-5 border border-cyan-200 [&>input]:bg-yellow-400 [&>input]:first-of-type:bg-cyan-400'> */}
+        <Places
+          setOffice={position => {
+            setOffice(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        {/* </div> */}
+      </div>
+      {/* <GoogleMap zoom={12} center={bangkokCords} mapContainerClassName='map' options={options} /> */}
+      <GoogleMap zoom={12} center={bangkokCords} mapContainerClassName='map' options={options} />
+    </div>
+  );
 };
--- apps/app/src/app/(protected)/events/create/form/event-location/index.tsx
+++ apps/app/src/app/(protected)/events/create/form/event-location/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MapLocation } from './MapLocation';
 import { UseFormRegister } from 'react-hook-form';
+import { useLoadScript } from '@react-google-maps/api';
+import { LoaderSpinner } from '__components/ui/LoaderSpinner';
 
 type Props = {
   register: UseFormRegister<any>;
@@ -8,11 +10,24 @@ type Props = {
   // errors: any;
 };
 
+type Library = 'places';
+
+const libraries: Library[] = ['places'];
+
 export const EventLocation = ({ register }: Props) => {
-  return (
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY,
+    libraries: libraries,
+  });
+
+  return isLoaded ? (
     <div>
-      index
       <MapLocation />
     </div>
+  ) : (
+    <>
+      Map
+      <LoaderSpinner />
+    </>
   );
 };
