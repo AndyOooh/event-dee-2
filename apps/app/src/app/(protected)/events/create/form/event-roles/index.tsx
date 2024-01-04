f808e42c3e33af946ba8d85990fdc5d4e7b667f2
AndyOoh <andy.oeee@gmail.com>
2024-01-04 22:26:45 +0700

    redesign

--- apps/app/src/app/(protected)/events/create/form/event-roles/index.tsx
+++ apps/app/src/app/(protected)/events/create/form/event-roles/index.tsx
@@ -21,7 +21,36 @@ export const EventRoles = ({ register, errors }: Props) => {
     <div className={styles.form}>
       {/* <div className='w-full grid grid-cols-2 gap-6'> */}
       <div className='w-full flex gap-6'>
-        {formArrayEventRoles.slice(0, 7).map(info => (
+        {formArrayEventRoles.slice(0, 6).map(info => (
+          <div key={info.title}>
+            {info.type === 'select' ? (
+              <Select
+                name={info.title}
+                defaultValue={info.defaultValue}
+                options={info.options}
+                register={register}
+                label={true}
+                className='select-sm '
+                maxW='max-w-md'
+              />
+            ) : (
+              <TextInput
+                name={info.title}
+                defaultValue={info.defaultValue}
+                register={register}
+                label={true}
+                className={`${info.className} input-sm`}
+                // maxW='max-w-md'
+                prepend={info.prepend}
+                prependClassName={info.prependClassName}
+              />
+            )}
+            <FormError formError={errors?.[info.title]?.message as string} />
+          </div>
+        ))}
+      </div>
+      <div className='w-full flex gap-6'>
+        {formArrayEventRoles.slice(6, 8).map(info => (
           <div key={info.title}>
             {info.type === 'select' ? (
               <Select
