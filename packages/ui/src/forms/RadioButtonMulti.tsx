bdcb030b2a4e782181f63f2f0fdc25eb0bc5e143
AndyOoh <andy.oeee@gmail.com>
2024-01-09 22:46:11 +0700

    onclicked, checked

--- packages/ui/src/forms/RadioButtonMulti.tsx
+++ packages/ui/src/forms/RadioButtonMulti.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { Controller, Control, UseFormRegister } from 'react-hook-form';
 
 type Props = {
@@ -9,15 +10,18 @@ type Props = {
   options: string[];
 };
 
+/* Default checked is options[0] */
 export const RadioButtonMulti = ({ control, register, name, options }: Props) => {
   const title = name.replace(/_/g, ' ');
+
+  const [checked, setChecked] = useState<string>(options[0]);
 
   return (
     <div className='flex flex-col gap-2 justify-center items-center'>
       <label className='label flex-col'>
         {title}
         <div className='flex'>
-          {options.map(option => (
+          {options.map((option, index) => (
             <div key={option} className='form-control'>
               <label className='label gap-2 cursor-pointer'>
                 <span className='label-text'>{option}</span>
@@ -28,9 +32,11 @@ export const RadioButtonMulti = ({ control, register, name, options }: Props) =>
                   render={({ field }) => (
                     <input
                       type='radio'
+                      onClick={() => setChecked(option)}
                       {...field}
                       value={option}
                       className='radio-xs checked:bg-red-500'
+                      checked={checked === option}
                     />
                   )}
                 />
