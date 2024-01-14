040846a95c2133c1e1e880e1606f21a01561eb96
AndyOoh <andy.oeee@gmail.com>
2024-01-14 21:58:10 +0700

    cleanup layout events

--- apps/app/src/app/(protected)/events/layout.tsx
+++ apps/app/src/app/(protected)/events/layout.tsx
@@ -1,4 +1,3 @@
 export default function EventsLayout({ children }: { children: React.ReactNode }) {
-  // return <div className='bg-orange-200'>{children}</div>;
   return <div className='w-full'>{children}</div>;
 }
