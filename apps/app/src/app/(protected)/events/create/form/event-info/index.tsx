a3015cf469950ca37e739164e60ecc65d0eafc4d
AndyOoh <andy.oeee@gmail.com>
2024-01-16 23:53:22 +0700

    restruicture layout

--- apps/app/src/app/(protected)/events/create/form/event-info/index.tsx
+++ apps/app/src/app/(protected)/events/create/form/event-info/index.tsx
@@ -16,11 +16,13 @@ type Props = {
 
 export const EventInfo = ({ register, errors }: Props) => {
   const { currentUser } = useContext(CurrUserContext);
+  const dateIndex = 4;
+  const descriptionIndex = 5;
 
   return currentUser ? (
     <div className={styles.form}>
       <div className='w-full grid grid-cols-2 gap-6'>
-        {formArrayEventDetails.slice(0, 4).map(info => (
+        {formArrayEventDetails.slice(0, dateIndex).map(info => (
           <div key={info.title}>
             {info.type === 'select' ? (
               <Select
@@ -30,15 +32,6 @@ export const EventInfo = ({ register, errors }: Props) => {
                 register={register}
                 label={true}
                 maxW='max-w-md'
-              />
-            ) : info.type === 'date' ? (
-              <DatePicker
-                name={info.title}
-                defaultValue={currentUser && formatDate(currentUser[info.title], true)}
-                register={register}
-                label={true}
-                maxW='max-w-md'
-                extraProps={info.extraProps}
               />
             ) : (
               <TextInput
@@ -54,11 +47,25 @@ export const EventInfo = ({ register, errors }: Props) => {
             {/* <FormError formError={errors?.[info.title]?.message} /> */}
           </div>
         ))}
+        <div className='flex justify-between'>
+          {formArrayEventDetails.slice(dateIndex, descriptionIndex).map(info => (
+            <div key={info.title}>
+              <DatePicker
+                name={info.title}
+                defaultValue={currentUser && formatDate(currentUser[info.title], true)}
+                register={register}
+                label={true}
+                maxW='max-w-md'
+                extraProps={info.extraProps}
+              />
+            </div>
+          ))}
+        </div>
       </div>
       <div className='w-1/2 mx-auto'>
         <TextInput
-          name={formArrayEventDetails[4].title}
-          defaultValue={currentUser && currentUser[formArrayEventDetails[4].title]}
+          name={formArrayEventDetails[descriptionIndex].title}
+          defaultValue={currentUser && currentUser[formArrayEventDetails[descriptionIndex].title]}
           register={register}
           label={true}
           maxW='max-w-md'
