104b3167eaa00e3b4fdb690a7f03493b16ca65f8
AndyOoh <andy.oeee@gmail.com>
2024-01-21 18:06:13 +0700

    delete step3 company signup

--- apps/app/src/app/(public)/(auth)/signup/business/SignupBusinessRight/SignupBusinessRight.tsx
+++ apps/app/src/app/(public)/(auth)/signup/business/SignupBusinessRight/SignupBusinessRight.tsx
@@ -1,13 +1,10 @@
 'use client';
 
 import { useRecoilState } from 'recoil';
-
 import { wizardForm } from '__atoms/signupBusinessAtom';
-// import { RightSide } from 'app/(public)/(auth)/components/RightSide';
 import { ChangeEventHandler } from 'react';
 import { Step1 } from './steps/Step1';
 import { Step2 } from './steps/Step2';
-import { Step3 } from './steps/Step3';
 import { RightSide } from '../../../components/RightSide';
 
 export type OnChange = ChangeEventHandler<HTMLInputElement>;
@@ -25,7 +22,7 @@ export const SignupBusinessRight = () => {
 
   return (
     <RightSide step={step} header={header}>
-      {step === 1 ? <Step1 /> : step === 2 ? <Step2 /> : <Step3 />}
+      {step === 1 ? <Step1 /> : <Step2 />}
     </RightSide>
   );
 };
