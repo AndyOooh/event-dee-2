e73e38305cfa48b7315ef9acd5893c547817ecaa
AndyOoh <andy.oeee@gmail.com>
2024-01-28 19:56:50 +0700

    Events metadata

--- apps/app/src/app/(protected)/events/layout.tsx
+++ apps/app/src/app/(protected)/events/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Event Dee - Events',
+};
+
 export default function EventsLayout({ children }: { children: React.ReactNode }) {
   return <div className='w-full'>{children}</div>;
 }
