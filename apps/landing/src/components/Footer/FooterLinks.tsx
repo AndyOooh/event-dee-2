b1937dcd95a8a11afa04cf294a83c88fbf00afc3
AndyOoh <andy.oeee@gmail.com>
2024-02-03 00:41:11 +0700

    fix key, typo

--- apps/landing/src/components/Footer/FooterLinks.tsx
+++ apps/landing/src/components/Footer/FooterLinks.tsx
@@ -20,55 +20,55 @@ export const FooterLinks = (props: Props) => {
     {
       title: 'Articles',
       href: '#',
-      extarnel: false,
+      external: false,
       logo: <RiArticleFill size={logoSize} />,
     },
     {
       title: 'Twitter',
       href: '#',
-      extarnel: true,
+      external: true,
       logo: <SiTwitter size={logoSize} />,
     },
     {
       title: 'Discord',
       href: '#',
-      extarnel: true,
+      external: true,
       logo: <SiDiscord size={logoSize} />,
     },
     {
       title: 'Whitepaper',
       href: '#',
-      extarnel: false,
+      external: false,
       logo: <SiReadthedocs size={logoSize} />,
     },
     {
       title: 'LinkedIn',
       href: '#',
-      extarnel: true,
+      external: true,
       logo: <SiLinkedin size={logoSize} />,
     },
     {
       title: 'Youtube',
       href: '#',
-      extarnel: true,
+      external: true,
       logo: <SiYoutube size={logoSize} />,
     },
   ];
   return (
     <div className='flex flex-col gap-4 py-10'>
       <div className='flex justify-between gap-16 w-5/6 mx-auto'>
-        {links.slice(0, 3).map(link => {
+        {/* {links.slice(0, 3).map((link, index) => {
           return (
             <>
               <div
-                key={link.title}
+                key={link.title + index}
                 className='flex justify-between w-full pb-4 border-b border-base-content/50'>
                 <a href={link.href} className='flex items-center justify-between w-full'>
                   <div className='flex items-center justify-center gap-4'>
                     {link.logo}
                     <p>{link.title}</p>
                   </div>
-                  {link.extarnel ? (
+                  {link.external ? (
                     <RxArrowTopRight size={logoSize} />
                   ) : (
                     <RxArrowRight size={logoSize} />
@@ -77,28 +77,24 @@ export const FooterLinks = (props: Props) => {
               </div>
             </>
           );
-        })}
+        })} */}
       </div>
       <div className='flex justify-between gap-16 w-5/6 mx-auto'>
-        {links.slice(3).map(link => {
-          return (
-            <>
-              <div key={link.title} className='flex justify-between w-full pb-4'>
-                <a href={link.href} className='flex items-center justify-between w-full'>
-                  <div className='flex items-center justify-center gap-4'>
-                    {link.logo}
-                    <p>{link.title}</p>
-                  </div>
-                  {link.extarnel ? (
-                    <RxArrowTopRight size={logoSize} />
-                  ) : (
-                    <RxArrowRight size={logoSize} />
-                  )}
-                </a>
+        {links.slice(3).map((link, index) => (
+          <div key={link.title + index} className='flex justify-between w-full pb-4'>
+            <a href={link.href} className='flex items-center justify-between w-full'>
+              <div className='flex items-center justify-center gap-4'>
+                {link.logo}
+                <p>{link.title}</p>
               </div>
-            </>
-          );
-        })}
+              {link.external ? (
+                <RxArrowTopRight size={logoSize} />
+              ) : (
+                <RxArrowRight size={logoSize} />
+              )}
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
