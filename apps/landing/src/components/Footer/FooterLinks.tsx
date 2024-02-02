21d1e16fd38ef707c2ce22b21bf3e9cacc6d222b
AndyOoh <andy.oeee@gmail.com>
2024-02-03 01:17:56 +0700

    create link comp

--- apps/landing/src/components/Footer/FooterLinks.tsx
+++ apps/landing/src/components/Footer/FooterLinks.tsx
@@ -11,88 +11,93 @@ import {
   SiYoutube,
 } from 'react-icons/si';
 
-type Props = {};
+type Link = {
+  title: string;
+  href: string;
+  external: boolean;
+  logo: JSX.Element;
+};
 
-export const FooterLinks = (props: Props) => {
-  const logoSize = '1.75rem';
-  //   const linkLogoSize = '2rem';
-  const links = [
-    {
-      title: 'Articles',
-      href: '#',
-      external: false,
-      logo: <RiArticleFill size={logoSize} />,
-    },
-    {
-      title: 'Twitter',
-      href: '#',
-      external: true,
-      logo: <SiTwitter size={logoSize} />,
-    },
-    {
-      title: 'Discord',
-      href: '#',
-      external: true,
-      logo: <SiDiscord size={logoSize} />,
-    },
-    {
-      title: 'Whitepaper',
-      href: '#',
-      external: false,
-      logo: <SiReadthedocs size={logoSize} />,
-    },
-    {
-      title: 'LinkedIn',
-      href: '#',
-      external: true,
-      logo: <SiLinkedin size={logoSize} />,
-    },
-    {
-      title: 'Youtube',
-      href: '#',
-      external: true,
-      logo: <SiYoutube size={logoSize} />,
-    },
-  ];
+const logoSize = '1.75rem';
+const links: Link[] = [
+  {
+    title: 'Articles',
+    href: '#',
+    external: false,
+    logo: <RiArticleFill size={logoSize} />,
+  },
+  {
+    title: 'Twitter',
+    href: '#',
+    external: true,
+    logo: <SiTwitter size={logoSize} />,
+  },
+  {
+    title: 'Discord',
+    href: '#',
+    external: true,
+    logo: <SiDiscord size={logoSize} />,
+  },
+  {
+    title: 'Whitepaper',
+    href: '#',
+    external: false,
+    logo: <SiReadthedocs size={logoSize} />,
+  },
+  {
+    title: 'LinkedIn',
+    href: '#',
+    external: true,
+    logo: <SiLinkedin size={logoSize} />,
+  },
+  {
+    title: 'Youtube',
+    href: '#',
+    external: true,
+    logo: <SiYoutube size={logoSize} />,
+  },
+];
+// const LinkComponent = ({ link }: { link: Link }) => {
+//   return (
+//     <a href={link.href} className='flex items-center justify-between w-full'>
+//       <div className='flex items-center justify-center gap-4'>
+//         {link.logo}
+//         <p>{link.title}</p>
+//       </div>
+//       {link.external ? <RxArrowTopRight size={logoSize} /> : <RxArrowRight size={logoSize} />}
+//     </a>
+//   );
+// };
+
+export const FooterLinks = () => {
+  const linkComponent = (link: Link) => {
+    return (
+      <a href={link.href} className='flex items-center justify-between w-full'>
+        <div className='flex items-center justify-center gap-4'>
+          {link.logo}
+          <p>{link.title}</p>
+        </div>
+        {link.external ? <RxArrowTopRight size={logoSize} /> : <RxArrowRight size={logoSize} />}
+      </a>
+    );
+  };
   return (
     <div className='flex flex-col gap-4 py-10'>
       <div className='flex justify-between gap-16 w-5/6 mx-auto'>
-        {/* {links.slice(0, 3).map((link, index) => {
-          return (
-            <>
-              <div
-                key={link.title + index}
-                className='flex justify-between w-full pb-4 border-b border-base-content/50'>
-                <a href={link.href} className='flex items-center justify-between w-full'>
-                  <div className='flex items-center justify-center gap-4'>
-                    {link.logo}
-                    <p>{link.title}</p>
-                  </div>
-                  {link.external ? (
-                    <RxArrowTopRight size={logoSize} />
-                  ) : (
-                    <RxArrowRight size={logoSize} />
-                  )}
-                </a>
-              </div>
-            </>
-          );
-        })} */}
+        {links.slice(0, 3).map((link, index) => (
+          <div
+            key={link.title + index}
+            className='flex justify-between w-full pb-4 border-b border-base-content/50'>
+            {/* <LinkComponent link={link} /> */}
+            {linkComponent(link)}
+          </div>
+        ))}
       </div>
       <div className='flex justify-between gap-16 w-5/6 mx-auto'>
         {links.slice(3).map((link, index) => (
           <div key={link.title + index} className='flex justify-between w-full pb-4'>
-            <a href={link.href} className='flex items-center justify-between w-full'>
-              <div className='flex items-center justify-center gap-4'>
-                {link.logo}
-                <p>{link.title}</p>
-              </div>
-              {link.external ? (
-                <RxArrowTopRight size={logoSize} />
-              ) : (
-                <RxArrowRight size={logoSize} />
-              )}
-            </a>
+            {/* <LinkComponent link={link} /> */}
+            {linkComponent(link)}
           </div>
         ))}
       </div>
