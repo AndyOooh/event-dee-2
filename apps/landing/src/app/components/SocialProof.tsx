3d5a7fdcdd70d6e35d9eead7105bb97c5017fb9b
AndyOoh <andy.oeee@gmail.com>
2024-02-03 00:24:33 +0700

    fix image comp

--- apps/landing/src/app/components/SocialProof.tsx
+++ apps/landing/src/app/components/SocialProof.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import { SiBentley, SiBmw, SiLamborghini, SiMercedes, SiPorsche } from 'react-icons/si';
 
 import bmw from '../../../public/carPngs/bmw.png';
 import bentley from '../../../public/carPngs/bentley.png';
 import jaguar from '../../../public/carPngs/jaguar.png';
 import lamborghini from '../../../public/carPngs/lamborghini.png';
-import mercedes2 from '../../../public/carPngs/mercedes2.png';
-import mercedes from '../../../public/carPngs/mercedes.png';
+import mercedes from '../../../public/carPngs/mercedes2.png';
 import porsche from '../../../public/carPngs/porsche.png';
+import Image from 'next/image';
 
 // import hero from '../../public/hero/yeshi-kangrang-iuqxv7kFj64-unsplash.jpg';
 
 type Props = {};
 
 export const SocialProof = (props: Props) => {
+  console.log('🚀  props:', props);
   const logoSize = '4rem';
   const companies = [
     {
@@ -24,10 +24,8 @@ export const SocialProof = (props: Props) => {
     {
       name: 'Mercedes',
       logo: <SiMercedes size={logoSize} />,
-      imgUrl: mercedes2,
-      // imgUrl: mercedes,
+      imgUrl: mercedes,
     },
-
     {
       name: 'Porsche',
       logo: <SiPorsche size={logoSize} />,
@@ -53,7 +51,18 @@ export const SocialProof = (props: Props) => {
     <div className='flex w-full justify-around items-center'>
       {companies.map(company => {
         return (
-          <img key={company.name} src={company.imgUrl.src} alt={company.name} className='h-12 md:h-16 lg:h-20' />
+          <div key={company.name} className='relative h-12 md:h-16 lg:h-20 w-full'>
+            <Image
+              src={company.imgUrl}
+              // src={porsche}
+              alt={company.name}
+              fill={true}
+              // width={100}
+              // height={200}
+              // className='h-12 md:h-16 lg:h-20'
+              className='object-contain'
+            />
+          </div>
         );
       })}
     </div>
