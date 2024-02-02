b3e5ca93cc9cce5067f49bd8283b4393b5dad6fa
AndyOoh <andy.oeee@gmail.com>
2024-02-03 02:07:26 +0700

    lang in root layout

--- apps/landing/src/components/Header/HeaderRight.tsx
+++ apps/landing/src/components/Header/HeaderRight.tsx
@@ -1,16 +1,15 @@
-'use client';
-
 import Link from 'next/link';
 import { appUrl } from '../../consts';
 
-type Props = {};
-
-function HeaderRight({}: Props) {
+function HeaderRight() {
   return (
     <>
       <div className='flex gap-2 navbar-end'>
-        {/* <Link role='button' href={'signup/business'} className='btn btn-sm rounded-3xl normal-case'> */}
-        <Link role='button' href={appUrl} target='_blank'  className='btn btn-neutral btn-sm rounded-3xl normal-case'>
+        <Link
+          role='button'
+          href={appUrl}
+          target='_blank'
+          className='btn btn-neutral btn-sm rounded-3xl normal-case'>
           Hire Talent
         </Link>
       </div>
