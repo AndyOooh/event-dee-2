e8443db68dc2a7ad28ceeea946d2bcf66d716a20
AndyOoh <andy.oeee@gmail.com>
2024-02-03 18:26:16 +0700

    responsive navbar

--- apps/landing/src/components/Header/index.tsx
+++ apps/landing/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ type Props = {};
 function index({}: Props) {
   return (
     <header className='bg-base-300 fixed z-50 top-0 left-0 w-full'>
-      <nav className={`navbar w-5/6 px-0 gap-4 font-semibold ${container}`}>
+      <nav className={`navbar px-0 gap-4 font-semibold md:${container}`}>
         <HeaderLeft />
         <NavBar />
         <HeaderRight />
