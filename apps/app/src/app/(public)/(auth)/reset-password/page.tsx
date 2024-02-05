0c653916afca839700150ce1dcdcad55c2514545
AndyOoh <andy.oeee@gmail.com>
2024-02-06 01:32:28 +0700

    metadata

--- apps/app/src/app/(public)/(auth)/reset-password/page.tsx
+++ apps/app/src/app/(public)/(auth)/reset-password/page.tsx
@@ -1,10 +1,15 @@
 import { styles } from '__styles/styles';
+import { Metadata } from 'next';
 import { ComingSoon } from 'ui';
+
+export const metadata: Metadata = {
+  title: 'Reset Password',
+};
 
 export default function ResetPasswordPage() {
   return (
     <div>
-      <ComingSoon pageName='Companies' containerClass={styles.innerWidth} />
+      <ComingSoon pageName='Reset Password' containerClass={styles.innerWidth} />
     </div>
   );
 }
