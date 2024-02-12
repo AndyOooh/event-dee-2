3959f32c61ba5c1ff627e1439bc661a4ceb484c1
AndyOoh <andy.oeee@gmail.com>
2024-02-12 22:30:46 +0700

    split into listeners and callable

---
+++ apps/firebase-cloud-functions/src/auth/listeners.ts
@@ -0,0 +1,32 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { db } from '..';
+
+/*
+ * listener - on create user (auth)
+ * Creates a new user doc
+ */
+export const onCreateUser = functions.auth.user().onCreate(async (user: any, ctx) => {
+  db.collection('users')
+    .doc(user.uid)
+    .set(JSON.parse(JSON.stringify(user)));
+});
+
+/*
+ * Listener - on delete user (auth)
+ * Deletes user doc
+ */
+export const deleteUser = functions.auth.user().onDelete(async (user: any) => {
+  try {
+    console.log('deleting user..: user.uid ');
+    // TODOD: delete user folder in storage. Also, is user deleted in auth??
+    await db.collection('users').doc(user.uid).delete();
+
+    await admin
+      .storage()
+      .bucket()
+      .deleteFiles({ prefix: `users/${user.uid}` });
+  } catch (error) {
+    console.log('🚀  file: crud-user.ts:68  error:', error);
+  }
+});
