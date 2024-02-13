3f7ae73d4228108d441f661b1242fc54df040ce5
AndyOoh <andy.oeee@gmail.com>
2024-02-13 22:58:57 +0700

    add http test func

--- apps/firebase-cloud-functions/src/index.ts
+++ apps/firebase-cloud-functions/src/index.ts
@@ -11,3 +11,11 @@ export const db = admin.firestore();
 export const test = functions.https.onRequest((req, res) => {
   res.send('Hello from Firebase!');
 });
+
+export const testDebugger = functions.https.onRequest((req, res) => {
+  const a = 22;
+  const b = 33;
+  const c = a + b;
+  console.log('🚀  c:', c);
+  res.send({ message: 'Hello from Firebase!', data: c });
+});
