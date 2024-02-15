36aba359c32bee8238e0fcfdf2350986619690ad
AndyOoh <andy.oeee@gmail.com>
2024-02-15 23:35:31 +0700

    move to old-unused

--- packages/ui/src/forms/index.tsx
+++ packages/ui/src/forms/index.tsx
@@ -1,12 +1,13 @@
 export * from './Button';
 export * from './FormError';
 
+// export { SearchableSelect } from './old-unused/SearchableSelect';
+// export { SearchableSelect2 } from './old-unused/SearchableSelect2';
+// export { ReactSelect } from './old-unusedReactSelect';
+
 export { TextInput } from './TextInput';
 export { Checkbox } from './Checkbox';
 export { Select } from './Select';
-export { SearchableSelect } from './SearchableSelect';
-export { SearchableSelect2 } from './SearchableSelect2';
-export { ReactSelect } from './ReactSelect';
 export { DatePicker } from './DatePicker';
 export { ActionButton } from './ActionButton';
 export { RadioButtonMulti } from './RadioButtonMulti';
