dc185ca2683186840ba8b5df3d5c63aec68ec5ae
AndyOoh <andy.oeee@gmail.com>
2024-05-07 02:44:38 +0700

    test changes

--- apps/firebase-cloud-functions/src/index.ts
+++ apps/firebase-cloud-functions/src/index.ts
@@ -9,12 +9,12 @@ admin.initializeApp();
 export const db = admin.firestore();
 
 export const test = functions.https.onRequest((req, res) => {
-  res.send('Hello from Firebase!');
+  res.send('Hello from Firebase 888!');
 });
 
 export const testDebugger = functions.https.onRequest((req, res) => {
   const a = 22;
-  const b = 33;
+  const b = 138;
   const c = a + b;
   console.log('🚀  c:', c);
   res.send({ message: 'Hello from Firebase!', data: c });
