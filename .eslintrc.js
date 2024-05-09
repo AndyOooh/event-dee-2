c0f2059715155d865b06ac89c4c2348ef5c2db9a
AndyOoh <andy.oeee@gmail.com>
2024-05-09 21:47:31 +0700

    update eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
+// This configuration only applies to the package manager root.
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
+  /* OLD */
+  // extends: ["custom"],
+  // root: true,
+  // settings: {
+  //   next: {
+  //     rootDir: ["apps/*/"],
+  //   },
+  // },
+
+  /* NEW */
+  ignorePatterns: ["apps/**", "packages/**"],
+  extends: ["@repo/eslint-config/library.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
   },
 };
